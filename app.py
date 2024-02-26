294b3d99f9ab12457a97946e12d2d1c45984d6a8
prasanth <prasanth300don@gmail.com>
2024-02-26 14:39:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,7 @@ sqlconnection.close()
 def home():
     return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+
 
 @app.route('/aerators')
 def aerators():
